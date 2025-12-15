5927d0e2a67f720f5492421e2ffb26a7a69fc29d
Tatsuhiro Tsuchiya <t-tutiya@ist.osaka-u.ac.jp>
2025-12-15 12:19:25 +0900

    First commit

---
+++ cit-bach-01/src/main/java/jp/ac/osaka_u/cit_bach_01/App.java
@@ -0,0 +1,10 @@
+package jp.ac.osaka_u.cit_bach_01;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
