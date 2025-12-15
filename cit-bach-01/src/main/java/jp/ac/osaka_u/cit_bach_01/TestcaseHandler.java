2309d5e39f376f90ef9abdb2adb4da818f5c9f73
Tatsuhiro Tsuchiya <t-tutiya@ist.osaka-u.ac.jp>
2025-12-15 13:35:58 +0900

    all files restored from the OSDN repository

---
+++ cit-bach-01/src/main/java/jp/ac/osaka_u/cit_bach_01/TestcaseHandler.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package jp.ac.osaka_u.cit_bach_01;
+
+/**
+ * @author Tsuchiya
+ *
+ */
+
+// import java.io.BufferedWriter;
+// import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TestcaseHandler {
+	final int numOfParameters;
+
+	TestcaseHandler(int numOfParameters) {
+		this.numOfParameters = numOfParameters;
+	}
+
+	Testcase getTestcase() {
+		return new Testcase(numOfParameters);
+	}
+}
+
+
+class Testsuite {
+	List<Testcase> test;
+
+	Testsuite() {
+		test = new LinkedList<Testcase>();
+	}
+
+	void add(Testcase newtest) {
+		this.test.add(newtest);
+	}
+}
